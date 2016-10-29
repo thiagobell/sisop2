2bbf9fdbab3a5542ba5f76a3dbc5164546ef482e
Thiago Bell <thiago@tim>
2016-10-29 20:09:13 -0200

    fixed makefile

--- t3/src/shared/message.c
+++ t3/src/shared/message.c
@@ -1,8 +1,121 @@
 #include "message.h"
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 unsigned int get_message_length(char *pointer)
 {
     MESSAGE *p = (MESSAGE*) pointer;
     return p->length;
+}
+
+
+int read_message_from_socket(int sock, MESSAGE *msg) {
+
+    char buff[100];
+    //reads first 12 bytes (HEADER)
+    int nbytes = 0, ret;
+    while(nbytes < 12) {
+        ret = recv(sock, buff+nbytes, MESSAGE_HEADER_SIZE-nbytes, 0);
+        if(ret == -1) {
+            //problem with sock
+            return -1;
+        } else if(ret == 0) {
+            //client disconnected
+            return -2;
+        }
+        nbytes += ret;
+    }
+    msg->type = *((uint32_t*) buff);
+    msg->length = *((uint32_t*) (buff+4));
+    msg->sequence_number = *((uint32_t*) (buff+8));
+
+    if(msg->length > 0) {
+        nbytes = nbytes - MESSAGE_HEADER_SIZE; //accounts for header
+
+        char *payload = (char*) calloc(sizeof(char), msg->length);
+        if(nbytes > 0) {
+            memcpy(payload, buff+MESSAGE_HEADER_SIZE, nbytes);
+        }
+
+        while(nbytes < msg->length) {
+            ret = recv(sock, payload+nbytes, msg->length-nbytes, 0);
+            if(ret < 0) {
+                printf("problem with sock");
+
+                return -1;
+            } else if(ret == 0) {
+                printf("client disconnected");
+                return -2;
+            }
+            nbytes += ret;
+        }
+        msg->data = payload;
+    } else {
+        msg->data = NULL;
+    }
+
+    printf("Message of type %d and length %d received with sequence number= %d\n", msg->type, msg->length,msg->sequence_number);
+
+    return 0;
+}
+
+int send_message_to_socket(int sock, int type, int length, int sequence_number, char *data) {
+        MESSAGE msg;
+        msg.type = type;
+        msg.length = length;
+        msg.sequence_number = sequence_number;
+        msg.data = data;
+
+
+        int bytes = 0, ret;
+        while(bytes < 4) {
+            ret = send(sock , ((char*)&msg.type)+bytes , sizeof(msg.type)-bytes, 0);
+            if(ret == -1) {
+                //problem with socket
+                printf("send error1\n");
+                return -1;
+            }
+            bytes += ret;
+        }
+        bytes = 0;
+        while(bytes < 4) {
+            ret = send(sock , ((char*)&msg.length)+bytes , sizeof(msg.length)-bytes , 0);
+
+            if(ret == -1) {
+                //problem with socket
+                printf("send error2\n");
+                return -1;
+            }
+            bytes += ret;
+        }
+
+        bytes = 0;
+        while(bytes < 4) {
+            ret = send(sock , ((char*)&msg.sequence_number)+bytes , sizeof(msg.sequence_number)-bytes , 0);
+
+            if(ret == -1) {
+                //problem with socket
+                printf("send error3\n");
+                return -1;
+            }
+            bytes += ret;
+        }
+        if(msg.data != NULL) {
+            //has to send payload
+            bytes = 0;
+            while(bytes < msg.length) {
+                ret = send(sock , ((char*)msg.data)+bytes , msg.length - bytes , 0);
+                if(ret == -1) {
+                    //problem with socket
+                    printf("send error4\n");
+                    return -1;
+                }
+                bytes += ret;
+            }
+        }
+        return 0;
 }
--- t3/src/shared/message.h
+++ t3/src/shared/message.h
@@ -2,22 +2,76 @@
 #define MESSAGE_H
 #include <stdint.h>
 
-/* message types are defined here */
-#define MESSAGE_CHAT_MSG
-#define MESSAGE_ENTER_ROOM
-
-
 /* this module defines the message format for the chat application */
 
+
+/* message types are defined here */
+
+/* contains actual text messages
+ *   needs to carry payload
+ *  client does not confirm this type of message
+*/
+#define MESSAGE_CHAT_MSG_TO_SERVER 1
+#define MESSAGE_CHAT_MSG_TO_CLIENT 2
+
+
+/* enter room
+    needs payload: room name
+    if rooms does not exist, creates a room
+*/
+#define MESSAGE_ENTER_ROOM 3
+/* exit room*/
+#define MESSAGE_EXIT_ROOM 4
+
+/* request: list rooms*/
+#define MESSAGE_LIST_ROOMS_REQUEST 5
+/* reply: list rooms
+    carries payload. room names separated by \n
+*/
+#define MESSAGE_LIST_ROOMS_REPLY 6
+
+/* sets nick*/
+#define MESSAGE_NEW_NICK 7
+/* if nick is invalid server sends this message*/
+#define MESSAGE_ERROR_INVALID_NICK 8
+
+/* request was executued successfully */
+#define MESSAGE_OK 9
+
+/* sent when trying to enter room without nick*/
+#define MESSAGE_ERROR_NICK_NOT_SET 10
+/* sent when trying to enter a full room*/
+#define MESSAGE_ERROR_ROOM_IS_FULL 11
+/* sent when there is no more space for
+additional rooms*/
+#define MESSAGE_ERROR_MAX_ROOM_REACHED 11
+
+
+
+
+
+/* Since the answer to a request may arrive after
+ * another message sent by another client through
+ * another thread on the server, a sequence number
+ * is important to have. A reply with sequence number
+ * acknowledges receipt of request with the same number.
+*/
 typedef struct message {
     uint32_t type; /*message type*/
-    uint32_t length; /*length of whole message*/
+    uint32_t length; /*length of payload*/
+    uint32_t sequence_number; /* message number */
     char *data; /*payload*/
 }MESSAGE;
+
+/*size in bytes of message header*/
+#define MESSAGE_HEADER_SIZE 12
 
 
 /* to call this function there must be the first 64 bits of valid data on the buffer
 pointer by the functions paramenter. */
 unsigned int get_message_length(char *pointer);
+
+int read_message_from_socket(int sock, MESSAGE *msg);
+int send_message_to_socket(int sock, int type, int length, int sequence_number, char *data);
 
 #endif // MESSAGE_H
